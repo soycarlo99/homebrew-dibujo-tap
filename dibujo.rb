a5b3210476b20d53d8e4900d447a3ae8e2e609a8
carlodelos90 <carlodelos90@gmail.com>
2025-01-10 19:21:16 +0100

    Update SHA256 checksum for Dibujo v1.2

--- dibujo.rb
+++ dibujo.rb
@@ -1,7 +1,7 @@
 class Dibujo < Formula
   desc "SFML Drawing Tool with Features"
   homepage "https://github.com/soycarlo99/dibujo"
-  url "https://github.com/soycarlo99/dibujo/releases/tag/v1.2"
+  url "https://github.com/soycarlo99/dibujo/archive/v1.2.tar.gz"
   sha256 "b3ff1b77f33da60c5725fe4ee72a6d1c4cf0ba189e48fbe8eb1910c77caa040c"
   license "MIT"
 
