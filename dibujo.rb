701bd41efaf75881ba61b20ecfb69f81f77068c0
carlodelos90 <carlodelos90@gmail.com>
2025-01-10 19:27:37 +0100

    Switch to sfml@2 dependency in formula

--- dibujo.rb
+++ dibujo.rb
@@ -2,14 +2,14 @@ class Dibujo < Formula
   desc "SFML Drawing Tool with Features"
   homepage "https://github.com/soycarlo99/dibujo"
   url "https://github.com/soycarlo99/dibujo/archive/v1.2.tar.gz"
-  sha256 "b3ff1b77f33da60c5725fe4ee72a6d1c4cf0ba189e48fbe8eb1910c77caa040c"
+  sha256 "b3ff1b77f33da60c5725fe4ee72a6d1c4cf0ba189e48fbe8eb1910c77caa040c"  # Ensure this is correct for v1.2
   license "MIT"
 
-  depends_on "sfml@2.5"
+  depends_on "sfml@2"
 
   def install
-    ENV["CXXFLAGS"] = "-std=c++17 -I#{Formula["sfml@2.5"].opt_include}"
-    ENV["LDFLAGS"] = "-L#{Formula["sfml@2.5"].opt_lib} -lsfml-graphics -lsfml-window -lsfml-system"
+    ENV["CXXFLAGS"] = "-std=c++17 -I#{Formula["sfml@2"].opt_include}"
+    ENV["LDFLAGS"] = "-L#{Formula["sfml@2"].opt_lib} -lsfml-graphics -lsfml-window -lsfml-system"
     system "make"
     bin.install "dibujov0.1"
   end
