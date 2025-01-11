896a7401e007be8459cb0129107d338c38201ef1
carlodelos90 <carlodelos90@gmail.com>
2025-01-11 18:37:51 +0100

    updated checksum

--- dibujo.rb
+++ dibujo.rb
@@ -2,31 +2,35 @@ class Dibujo < Formula
   desc "SFML Drawing Tool with Features"
   homepage "https://github.com/soycarlo99/dibujo"
   url "https://github.com/soycarlo99/dibujo/archive/v1.6.tar.gz"
-  sha256 "33e6681f928da0c532ba03f0723b6a2fc6b649b5dcdcb160dbf7f4f7d94154ed"
+  sha256 "c10170fe420beb42a9b3164cad3a12c5d46a3914db0e2a12c0e3ed1093bff73b"
   license "MIT"
 
   depends_on "sfml@2"
 
   def install
     ENV["CXXFLAGS"] = "-std=c++17 -I#{Formula["sfml@2"].opt_include}"
-    ENV["LDFLAGS"] = "-L#{Formula["sfml@2"].opt_lib} -lsfml-graphics -lsfml-window -lsfml-system"
-    system "make"
-    bin.install "dibujov0.1"
+    ENV["LDFLAGS"]  = "-L#{Formula["sfml@2"].opt_lib} -lsfml-graphics -lsfml-window -lsfml-system"
 
-    (share/"dibujo")
-      bin.install "arial.ttf", "circle.png", "squares.png", "draw.png", "text.png"
+    # Build your app
+    system "make"
+
+    # Install the binary under a new name:
+    bin.install "dibujov0.1" => "dibujo"
+
+    # Install assets
+    (share/"dibujo").install "arial.ttf", "circle.png", "squares.png", "draw.png", "text.png"
   end
 
   test do
-    system "#{bin}/dibujov0.1", "--version"
+    system "#{bin}/dibujo", "--version"
   end
 
   def caveats
     <<~EOS
-      Thank you for installing Dibujo v1.4!
+      Thank you for installing Dibujo v1.6!
 
-      To run the program, execute:
-        dibujov0.1
+      To run the program, simply type:
+        dibujo
 
       For help and usage instructions, visit:
         https://github.com/soycarlo99/dibujo
