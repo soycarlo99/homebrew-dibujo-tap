e2a8dd0bb49f7b40f6e27d2da93b0d60ac307aa1
carlodelos90 <carlodelos90@gmail.com>
2025-01-13 17:58:52 +0100

    Update formula for version 0.10 with new checksum

--- dibujo.rb
+++ dibujo.rb
@@ -1,7 +1,7 @@
 class Dibujo < Formula
   desc "SFML Drawing Tool with Features"
   homepage "https://github.com/soycarlo99/dibujo"
-  url "https://codeload.github.com/soycarlo99/dibujo/tar.gz/refs/tags/v0.10"
+url "https://github.com/soycarlo99/dibujo/tar.gz/refs/tags/v0.10"
 sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
